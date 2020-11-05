6f47fab5e80e6d93b86c6c2b260a079ad77aa4bf
Tony Tambasco <tambascot@users.noreply.github.com>
2020-11-05 01:31:13 -0500

    Create FocalPoint.java

---
+++ FocalPoint.java
@@ -0,0 +1,22 @@
+package com.tonytambasco.lightplotter
+
+private class FocalPoint {
+
+  int pointNumber;  // a number for each focal point in the plot
+  String name;      // the name of the Focal Point
+  String notes;     // Misc notes about the FP
+  int pos_x;        // The X position of the FP
+  int pos_y;        // The Y position of the FP
+  int pos_z;        // The Z position of the FP
+  int height;       // The height in inches of the focal point. 
+  
+  private FocalPoint(int number) {
+    /* The only thing we need to define when creating a new focal point is its number. This should
+       be a UNIQUE integer that is tracked, incrimented, and checked by the calling method. But, in
+       practice, if the calling method wants to track a fixture in some other way, it can */
+    pointNumber = number;
+  }
+  
+  /* Methods for setting and getting values go here */
+
+}
