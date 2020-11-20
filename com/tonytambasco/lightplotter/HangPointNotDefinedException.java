fc37c15cf52b59fea5d40224234477d07113d3e1
tambascot <tambascot@yahoo.com>
2020-11-20 00:56:49 -0500

    Adding HangPointNotDefinedException

---
+++ com/tonytambasco/lightplotter/HangPointNotDefinedException.java
@@ -0,0 +1,7 @@
+package com.tonytambasco.lightplotter;
+
+public class HangPointNotDefinedException extends Exception {
+	public HangPointNotDefinedException(String message) {
+		super(message);
+	}
+}
