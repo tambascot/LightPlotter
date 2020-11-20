11b6ec0ee99583ebaa3957c898df2bfbd6341ecd
tambascot <tambascot@yahoo.com>
2020-11-20 00:53:51 -0500

    Adding FocalPointNotDefinedException

---
+++ com/tonytambasco/lightplotter/FocalPointNotDefinedException.java
@@ -0,0 +1,7 @@
+package com.tonytambasco.lightplotter;
+
+public class FocalPointNotDefinedException extends Exception {
+	public FocalPointNotDefinedException(String message) {
+		super(message);
+	}
+}
