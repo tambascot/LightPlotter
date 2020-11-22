770680505c0b8322e4e6224587762c910e3caaed
tambascot <tambascot@yahoo.com>
2020-11-22 17:12:09 -0500

    getters and setters for FocalPoint.java

--- com/tonytambasco/lightplotter/FocalPoint.java
+++ com/tonytambasco/lightplotter/FocalPoint.java
@@ -16,6 +16,64 @@ public class FocalPoint {
        practice, if the calling method wants to track a fixture in some other way, it can */
     this.fpNumber = fpNumber;
   }
+
+public int getFpNumber() {
+	return fpNumber;
+}
+
+public void setFpNumber(int fpNumber) {
+	this.fpNumber = fpNumber;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getNotes() {
+	return notes;
+}
+
+public void setNotes(String notes) {
+	this.notes = notes;
+}
+
+public int getPos_x() {
+	return pos_x;
+}
+
+public void setPos_x(int pos_x) {
+	this.pos_x = pos_x;
+}
+
+public int getPos_y() {
+	return pos_y;
+}
+
+public void setPos_y(int pos_y) {
+	this.pos_y = pos_y;
+}
+
+public int getPos_z() {
+	return pos_z;
+}
+
+public void setPos_z(int pos_z) {
+	this.pos_z = pos_z;
+}
+
+public int getHeight() {
+	return height;
+}
+
+public void setHeight(int height) {
+	this.height = height;
+}
+  
+  
   
   /* Methods for setting and getting values go here */
 
